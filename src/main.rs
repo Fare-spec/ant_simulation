624de9279c80b94f3bff8f5645c0de73ce7eedfa
Spectre <elouan.fare@protonmail.ch>
2024-12-06 22:22:32 +0100

    add a feature to get a cell

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::vec;
+use std::{thread::sleep, vec};
+
+use rand::Rng;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 
@@ -9,15 +11,21 @@ enum Cell {
     Ant,
 }
 fn main() {
-    let mut test: Vec<Vec<Cell>> = create_grid(13);
-    test = modify_cell(1, 1, Cell::Nest, test);
+    let size: u32 = 13;
+    let mut test: Vec<Vec<Cell>> = create_grid(size);
+    test = modify_cell(12, 12, Cell::Nest, test);
+    test = put_random_food(test, size);
     display_grid(&test);
 
 }
 
-fn create_grid(size: usize) -> Vec<Vec<Cell>> {
-    let grille = vec![vec![Cell::Empty; size]; size];
+fn create_grid(size: u32) -> Vec<Vec<Cell>> {
+    let grille = vec![vec![Cell::Empty; size as usize]; size as usize];
     grille
+}
+
+fn get_cell(grid: &Vec<Vec<Cell>>, x: u32, y: u32)->Cell{
+    grid[y as usize][x as usize]
 }
 fn display_grid(grid: &Vec<Vec<Cell>>) {
     for row in grid {
@@ -36,4 +44,21 @@ fn display_grid(grid: &Vec<Vec<Cell>>) {
 fn modify_cell(x: u32, y: u32,value_cell: Cell, mut grid: Vec<Vec<Cell>> )->Vec<Vec<Cell>>{
     grid[y as usize][x as usize] = value_cell;
     grid
+}
+
+fn put_random_food(mut grid: Vec<Vec<Cell>>, size: u32)->Vec<Vec<Cell>>{
+    let mut i: u16 = 0;
+    loop{
+        
+        let x = rand::thread_rng().gen_range(0..size);
+        let y = rand::thread_rng().gen_range(0..size);
+        if get_cell(&grid, x, y) == Cell::Empty{
+            grid[y as usize][x as usize] = Cell::Food;
+            return grid;
+        }
+        i+=1;
+        if i as u32>= size*size{
+            return grid;
+        }
+    }
 }
